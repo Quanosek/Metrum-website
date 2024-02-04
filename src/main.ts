d836c0cd789b86de52f9209fadbedb7fc52f8ad5
quanosek <kubaklalo@gmail.com>
2024-02-05 00:57:05 +0100

    Added Google robots and algorithms support

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import VueGtag from "vue-gtag";
+
 import "the-new-css-reset/css/reset.css";
 import "./assets/main.scss";
 
 const app = createApp(App);
 
 app.use(router);
+
+if (!import.meta.env.DEV) {
+  app.use(VueGtag, {
+    config: {
+      id: import.meta.env.GOOGLE_ANALYTICS_PROPERTY_ID,
+    },
+  });
+}
+
 app.mount("#app");
