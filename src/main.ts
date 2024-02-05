6ab44d8b3ae7c2323eaa3c5b12836df1a3c485ae
quanosek <kubaklalo@gmail.com>
2024-02-05 01:23:07 +0100

    gtag definition fixed

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import VueGtag from "vue-gtag";
-
 import "the-new-css-reset/css/reset.css";
 import "./assets/main.scss";
 
 const app = createApp(App);
 
 app.use(router);
-
-if (!import.meta.env.DEV) {
-  app.use(VueGtag, {
-    config: {
-      id: import.meta.env.VITE_GOOGLE_ANALYTICS_ID,
-    },
-  });
-}
-
 app.mount("#app");
